851c7a2e36a25bfb920ad94bded6d8037c95ccc2
Renato Aquino <50811575+Renato4qu1n0@users.noreply.github.com>
2023-11-15 15:13:07 -0300

    Add files via upload

---
+++ Faculdade/src/Fundamentos/MateriasJavinhaNossoDeCadaDay.java
@@ -0,0 +1,18 @@
+package Fundamentos;
+
+/** JAVA DOC
+ * @author Renato Aquino <re4qu1n0@gmail.com>
+ * @since V1.0
+ *Esse documento irá servir para teste de códigos e será a documentação de tudo que eu
+ *aprender durante meu desenvolvimento com a linguagem Java.
+ *Espero que dentro de alguns meses eu consiga dominar bem e pleitear uma vaga na area de desenvolvimento java
+ */
+//COMENTARIO DE UMA LINHA
+/*
+*Comentario de várias linhas
+*/
+public class MateriasJavinhaNossoDeCadaDay {
+    public static void main(String[] args) {
+        System.out.println("Como funciona comentários no código");
+    }
+}
