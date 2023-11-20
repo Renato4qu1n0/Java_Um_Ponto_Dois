7974bb1cff15155f5e6a3ee96b2a537a722504f9
Renato Aquino <50811575+Renato4qu1n0@users.noreply.github.com>
2023-11-20 09:48:09 -0300

    Add files via upload

---
+++ Faculdade/src/Fundamentos/Import.java
@@ -0,0 +1,22 @@
+package Fundamentos;
+
+import javax.swing.*;
+import java.util.Date;
+
+public class Import {
+    public static void main(String[] args){
+        /*
+        O import serve para localizar as bibliotecas de pacotes da linguagem java, então no caso do codigo, foi necessário importar a data
+        para poder exibir o resultado da data.
+         */
+
+        String s = "bom dia";
+        System.out.println(s);
+
+        Date d = new Date();
+        System.out.println(d);
+
+        //JButton butao = new JButton();
+
+    }
+}
